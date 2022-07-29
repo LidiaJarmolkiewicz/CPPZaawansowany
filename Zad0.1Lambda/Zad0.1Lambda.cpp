93f1c78070d147ebf1d02c834d5129bde9aee666
LidiaJarmolkiewicz <lidiajarmolkiewicz@gmail.com>
2022-07-29 16:28:31 +0200

    poprawione

--- Zad0.1Lambda/Zad0.1Lambda.cpp
+++ Zad0.1Lambda/Zad0.1Lambda.cpp
@@ -4,7 +4,7 @@ void print()
 {
     std::cout << "Hello World!";
 }
-int Sum(int x, int y)
+int add(int x, int y)
 {
     return x+y;
     
@@ -12,15 +12,30 @@ int Sum(int x, int y)
 
 int main()
 {
+    //a
     print();
     std::cout << std::endl;
-   
-    int x = 3;
-    int y = 7;
-    std::cout << Sum(x, y) << std::endl;
 
-    auto1= []() {std::cout << "Hello World!"; };
-    auto2=[](unsigned int x, unsigned int y) {std::cout << (x + y); };
+    auto hello = []() {std::cout << "Hello World!"; };
+    hello();
+    std::cout << std::endl;
+    []() {std::cout << "Hello World XXX"; }();
+    std::cout << std::endl;
+    
+    //b
+    std::cout << "sum="<<add(7,9) << std::endl;
+        
+    auto sum =[](unsigned int x, unsigned int y) {return x+y; };
+    std::cout << "suma wynosi=" << sum(5,87) << std::endl;
+    
+    //wskazniki na funkcje
+    auto hellFun = print;//wskaznik na funkce przyuzyciu auto
+    hellFun();
+    void(*ptrName)() = print;
+    ptrName();//wywo³anie funkcji
+     int(*addPtrFun)(int,int)=add;
+     std::cout << "suma 2 liczb wynosi= " << add(9, 77);
+     return 0;
 
-  
+      
 }
